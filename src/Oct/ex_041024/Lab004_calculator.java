819d80e7da927d86d158f6e72a6a3a21f8fc1971
Mohammed Azharuddin <azhar6198az@gmail.com>
2024-10-07 16:56:52 +0530

    MY FILES

---
+++ src/Oct/ex_041024/Lab004_calculator.java
@@ -0,0 +1,18 @@
+package Oct.ex_041024;
+
+public class Lab004  {
+    public static void main (String[] args) {
+
+            int a = 20;
+            int b = 10;
+            int Add = a + b;
+            System.out.println("Addition of two number is =" + Add);
+            int Subtraction = a - b;
+            System.out.println("Subtraction of two number is =" + Subtraction);
+            int multiplication = a * b;
+            System.out.println("Multiplication of two number is =" + multiplication);
+            int division = a / b;
+            System.out.println("Division of two number is =" + division);
+
+        }
+    }
