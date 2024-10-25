b7138de4bb8736ad892d20b87e80f40f0c252ed2
Mohammed Azharuddin <azhar6198az@gmail.com>
2024-10-25 11:32:18 +0530

    MY FILES

---
+++ src/Oct/ex_241024_ForLoop/Lab094.java
@@ -0,0 +1,13 @@
+package Oct.ex_241024_ForLoop;
+
+public class Lab094 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
+                System.out.println("Five");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ src/Oct/ex_241024_ForLoop/Lab100.java
@@ -0,0 +1,14 @@
+package Oct.ex_241024_ForLoop;
+
+public class Lab100 {
+    public static void main(String[] args) {
+            // Continue
+            for (int i = 0; i < 50; i++) {
+                if(i == 5){
+                    continue; // skip below code, move to top
+                }
+                System.out.println(i);
+            }
+        }
+    }
+
