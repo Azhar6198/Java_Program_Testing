8e40791c45fd732763d100b198ac22abcedc4aed
Mohammed Azharuddin <azhar6198az@gmail.com>
2024-10-26 15:17:45 +0530

    MY FILES

---
+++ src/Oct/ex_261024_WhileLoop/Lab103.java
@@ -0,0 +1,23 @@
+package Oct.ex_261024_WhileLoop;
+
+public class Lab103 {
+    public static void main(String[] args) {
+        int Azhar = 1;
+
+        float pi = 3.14f;
+        // 10,000 lines
+        // Jr QA
+        pi = 5;
+        pi = 10;
+        System.out.println(pi);
+
+
+
+
+        while(modi<=10){
+            System.out.println("Azhar 10 Years");
+            Azhar++;
+        }
+
+    }
+}
