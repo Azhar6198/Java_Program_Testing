47cd7d57e85df69970660efed48113f2209b6e54
Mohammed Azharuddin <azhar6198az@gmail.com>
2024-10-27 11:23:30 +0530

    MY FILES

---
+++ src/Oct/ex_271024_While/Lab106.java
@@ -0,0 +1,11 @@
+package Oct.ex_271014_While;
+
+public class Lab106 {
+    public static void main(String[] args) {
+        int i1 = 10;
+        while (i1>=1){
+            System.out.println(i1);
+            i1--;
+        }
+    }
+}
